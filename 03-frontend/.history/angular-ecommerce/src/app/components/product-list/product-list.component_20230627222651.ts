57640d034bdbbcb42462c823d9b1fecac9a2957a
Xiaojing Deng <xiaojingdeng@Xiaojings-Air.attlocal.net>
2023-06-27 22:29:55 -0400

    create frontend with angular, generate product list component and service

---
+++ 03-frontend/.history/angular-ecommerce/src/app/components/product-list/product-list.component_20230627222651.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products: Product[] = [];
+   constructor(private productService: ProductService){}
+}
