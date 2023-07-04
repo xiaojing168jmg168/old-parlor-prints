e5adf5fe377024a0ceb2b19e333e076a210bd6a9
Xiaojing Deng <8redfishcreative@gmail.com>
2023-07-04 14:37:27 -0400

    add products category title

--- 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/entity/Product.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/entity/Product.java
@@ -48,4 +48,5 @@ public class Product {
     @Column(name="last_updated")
     @CreationTimestamp
     private Date lastUpdated;
+    
 }
--- 03-frontend/.history/angular-ecommerce/src/app/components/product-list/product-list.component_20230704135937.ts
+++ 03-frontend/.history/angular-ecommerce/src/app/components/product-list/product-list.component_20230704135937.ts
@@ -1,44 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
-import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
+
+
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [];
   currentCategoryId: number = 1;
   currentCategoryName: string = "";
-
+  // inject productService
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
+
   }
 
   listProducts() {
 
     // check if "id" parameter is available
+
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    
 
     if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-
-      // get the "name" param string
-      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+      this.currentCategoryName = this.route.snapshot.paramMap.get('categroyName')!;
     }
     else {
-      // not category id available ... default to category id 1
+      // not category id available  ... default to category id 1
       this.currentCategoryId = 1;
-      this.currentCategoryName = 'Books';
+      this.currentCategoryName = "Books";
     }
 
     // now get the products for the given category id
@@ -48,5 +50,4 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
-
 }
