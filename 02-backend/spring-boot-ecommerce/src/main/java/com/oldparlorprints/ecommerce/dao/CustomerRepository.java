c06ad6613149287d298cae630913d1e05677f7e6
Xiaojing Deng <8redfishcreative@gmail.com>
2023-08-11 10:57:20 -0400

    handle customer by email backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/dao/CustomerRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/dao/CustomerRepository.java
@@ -4,5 +4,6 @@ import com.oldparlorprints.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByEmail(String theEmail);
 
 }
