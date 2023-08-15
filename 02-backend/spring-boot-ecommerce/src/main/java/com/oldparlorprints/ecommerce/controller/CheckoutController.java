fca093b3525a7c5bd9ce9132ed1cd4d9ffc02a74
Xiaojing Deng <8redfishcreative@gmail.com>
2023-08-15 10:18:54 -0400

    refactoring backend config, hide customer info

--- 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,7 @@ import com.oldparlorprints.ecommerce.dto.PurchaseResponse;
 import com.oldparlorprints.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
+
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
