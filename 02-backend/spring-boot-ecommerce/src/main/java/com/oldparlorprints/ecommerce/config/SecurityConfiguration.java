dbb7db1eabad49afbcab1d48a842360ff56e5984
Xiaojing Deng <8redfishcreative@gmail.com>
2023-08-17 11:17:41 -0400

    order history backend security

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/oldparlorprints/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.oldparlorprints.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // protect endpoint /api/orders
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults()));
+
+        // add CORS filters
+        http.cors(Customizer.withDefaults());
+
+        // add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+        // force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+}
+
+
